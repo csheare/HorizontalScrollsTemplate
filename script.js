49fa80b8a092cffd58c58605dc6c05f034218460
Courtney Shearer <csheare@clemson.edu>
2018-12-15 23:14:36 -0500

    is this still fun

--- script.js
+++ script.js
@@ -1,32 +1,64 @@
+function sideContent() {
+    // only one so we can return [0]
+    return document.getElementsByClassName('side-content')[0];
+
+}
+function navBar() {
+    // only one so return [0]
+    return document.getElementsByTagName('nav')[0];
+}
+
+function collapseSideBar() {
+    sideContent().style.display = 'none';
+    navBar().style.width = '80vw';
+    navBar().style.left = '20vw';
+}
+function expandSideBar() {
+    sideContent().style.display = 'block';
+    // fix navbar, should match initial css
+    navBar().style.width = '80vw';
+    navBar().style.left = '20vw';
+
+}
+
 $(function () {
-    function showPage(n) {
-        $('body').unbind('mousewheel');
-        currPage += n;
-        currPage = currPage <= 0 ? 0 : currPage >= $page.length - 1 ? $page.length - 1 : currPage;
-        var displacment = window.innerWidth/2 * currPage;
-        $('.pages').css('transform', 'translateX(-' + displacment + 'px)');
-        setTimeout(function () {
-            $('body').bind('mousewheel', mouseEvent);
-        }, 800);
-        $('nav a.active').removeClass('active');
-        $($('a')[currPage]).addClass('active');
+    function showSlide(n) {
+  $('body').unbind('mousewheel');
+  currSlide += n;
+  currSlide = currSlide <= 0 ? 0 : currSlide >= $slide.length - 1 ? $slide.length - 1 : currSlide;
+
+  if (currSlide === 0) {
+      collapseSideBar();
+  } else {
+      expandSideBar();
+  }
+
+
+// ORIGINAL CODE
+  var displacment = window.innerWidth * currSlide;
+  $('.slides').css('transform', 'translateX(-' + displacment + 'px)');
+  setTimeout(function () {
+      $('body').bind('mousewheel', mouseEvent);
+  }, 800);
+  $('nav a.active').removeClass('active');
+  $($('a')[currSlide]).addClass('active');
     }
     function mouseEvent(e, delta) {
-        showPage(delta >= 0 ? 1 : -1);
-        e.preventDefault();
+  showSlide(delta >= 0 ? 1 : -1);
+  e.preventDefault();
     }
     $('nav a').click(function (e) {
-        var newpage = parseInt($(this).attr('href')[1]);
-        var diff = newpage - currPage - 1;
-        showPage(diff);
-        e.preventDefault();
+  var newslide = parseInt($(this).attr('href')[1]);
+  var diff = newslide - currSlide - 1;
+  showSlide(diff);
+  e.preventDefault();
     });
     $(window).resize(function () {
-        var displacment = window.innerWidth/2 * currPage;
-        $('.pages').css('transform', 'translateX(-' + displacment + 'px)');
+  var displacment = window.innerWidth * currSlide;
+  $('.slides').css('transform', 'translateX(-' + displacment + 'px)');
     });
-    var currPage = 0;
-    var $page = $('.page');
+    var currSlide = 0;
+    var $slide = $('.slide');
     $($('nav a')[0]).addClass('active');
     $('body').bind('mousewheel', mouseEvent);
 });
